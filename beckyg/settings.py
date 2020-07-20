d1141d4609aa3dd1848a2edf4d496c5470e174ae
Keystroke3 <keystroke33@gmail.com>
2020-07-20 12:55:17 +0000

    made some security changes

--- beckyg/settings.py
+++ beckyg/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-# import django_heroku
+import json
+
+with open('/etc/beckyg-config.json') as config_file:
+    config= json.load(config_file)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,10 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nqp^!xw481w$@-(rn81-j2$2d2gpb84t*!yvbkey(*yhxwrxj9'
+SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['45.76.133.112','siliconraven.co']
 
